69bf5db8d1ab2bb1d1e473adc635a7b4bcc71570
M40 <modev.404@gmail.com>
2025-03-16 19:08:56 +0300

    loadtasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-
-import React, { useEffect } from 'react';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './routes';
-import { Toaster } from 'sonner';
-import { ThemeProvider } from '@/components/theme/theme-provider';
-import useUser from './hooks/useUser';
-import { useTaskStore } from './store/taskStore';
+import React, { useEffect } from "react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+import { Toaster } from "sonner";
+import { ThemeProvider } from "@/components/theme/theme-provider";
+import useUser from "./hooks/useUser";
+import { useTaskStore } from "./store/taskStore";
 
 function App() {
   const { user } = useUser();
-  const { subscribeToTasks, unsubscribeFromTasks } = useTaskStore();
+  const { subscribeToTasks, unsubscribeFromTasks, loadTasks } = useTaskStore();
 
   useEffect(() => {
     if (user) {
       subscribeToTasks();
     }
-
+    loadTasks();
     return () => {
       unsubscribeFromTasks();
     };
