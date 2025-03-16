e93074ed66c40c9d8d2f94280956e7e4fcbe8a1a
M40 <modev.404@gmail.com>
2025-03-16 17:09:37 +0300

    remove unwanted

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-
 import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes";
 import { useEffect } from "react";
+import { RouterProvider } from "react-router-dom";
+import { Toaster as Sonner } from "sonner";
+import { router } from "./routes";
 import { cleanupTimerStore } from "./store/timerStore";
-
-const queryClient = new QueryClient();
 
 const App = () => {
   // Cleanup timer when app unmounts
@@ -19,13 +15,11 @@ const App = () => {
   }, []);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <RouterProvider router={router} />
-      </TooltipProvider>
-    </QueryClientProvider>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <RouterProvider router={router} />
+    </TooltipProvider>
   );
 };
 
