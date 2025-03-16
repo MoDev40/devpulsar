50606563175799c2d89fb708e924a706741aa1a0
M40 <modev.404@gmail.com>
2025-03-16 17:04:51 +0300

    refactor

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,17 @@
-
 import { createBrowserRouter } from "react-router-dom";
 import Auth from "@/pages/Auth";
 import NotFound from "@/pages/NotFound";
 import Index from "@/pages/Index";
-import { RequireAuth } from "@/context/AuthContext";
+import { RequireAuth } from "@/components/RequireAuth";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <RequireAuth><Index /></RequireAuth>,
+    element: (
+      <RequireAuth>
+        <Index />
+      </RequireAuth>
+    ),
     errorElement: <NotFound />,
   },
   {
